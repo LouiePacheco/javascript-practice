73c90f934ecf6b24988fc3f37732b4cf028b7985
Louis Pacheco III <Pacheco_Louis@rocketmail.com>
2021-09-10 13:24:03 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var title = document.querySelector("h1");
+title.addEventListener("mouseover", function () {
+  title.innerText = "Lets get Ready To Rumblllleeeeeee";
+  title.style.color = "magenta";
+});
